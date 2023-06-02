c9ac3e5a6b4fa51cd589c688a9b66ae8c86792eb
Vladyslav Zolotarov <zolotaryov.v.g@gmail.com>
2023-06-02 18:54:34 +0300

    Parser presetup

--- src/mlang.cpp
+++ src/mlang.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 
-int main(){
-    std::cout << "Hi";
+#include "antlr4-runtime.h"
+#include "../parser/MlangLexer.h"
+#include "../parser/MlangParser.h"
+
+using namespace std;
+using namespace antlr4;
+
+int main(int argc, const char* argv[]) {
+    std::ifstream stream;
+    stream.open("input.mlang");
+    
+    ANTLRInputStream input(stream);
+    MlangLexer lexer(&input);
+    CommonTokenStream tokens(&lexer);
+    MlangParser parser(&tokens);    
+    
+    MlangParser::FileContext* tree = parser.file();
 
     return 0;
 }
